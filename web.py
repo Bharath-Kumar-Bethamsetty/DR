8e8009a4a05302853a52e647b195942cafdafb6f
BHARATH KUMAR BETHAMSETTY <116716230+Bharath-Kumar-Bethamsetty@users.noreply.github.com>
2023-04-06 14:48:13 +0530

    Update web.py

--- web.py
+++ web.py
@@ -36,7 +36,7 @@ st.markdown(
 
 if choice == "Home":
     st.subheader("Welcome to the Diabetic Retinopathy Classification app.")
-    x = '''Diabetic retinopathy is a diabetes complication that affects the eyes. It is caused by damage to the blood vessels in the retina, which can lead to vision impairment or even blindness if left untreated.
+    y = '''Diabetic retinopathy is a diabetes complication that affects the eyes. It is caused by damage to the blood vessels in the retina, which can lead to vision impairment or even blindness if left untreated.
 
 To tackle this problem, various research and development projects have been undertaken to detect and diagnose diabetic retinopathy at an early stage. These projects utilize a range of techniques, including computer vision and machine learning, to analyze retinal images and identify signs of the disease.
 
@@ -46,7 +46,19 @@ Another approach involves using telemedicine to screen patients for diabetic ret
 
 Overall, these projects aim to improve early detection and diagnosis of diabetic retinopathy, which can lead to earlier treatment and better outcomes for patients. '''
 	
+    st.write(y)
+    st.write("-----------")
+    st.subheader("Identify signs of diabetic retinopathy in eye images")
+    st.write("Diabetic retinopathy is the leading cause of blindness in the working-age population of the developed world. It is estimated to affect over 93 million people.")
+    st.image("retina.jpg")
+    x = '''
+	The US Center for Disease Control and Prevention estimates that 29.1 million people in the US have diabetes and the World Health Organization estimates that 347 million people have the disease worldwide. Diabetic Retinopathy (DR) is an eye disease associated with long-standing diabetes. Around 40% to 45% of Americans with diabetes have some stage of the disease. Progression to vision impairment can be slowed or averted if DR is detected in time, however this can be difficult as the disease often shows few symptoms until it is too late to provide effective treatment.
+
+Currently, detecting DR is a time-consuming and manual process that requires a trained clinician to examine and evaluate digital color fundus photographs of the retina. By the time human readers submit their reviews, often a day or two later, the delayed results lead to lost follow up, miscommunication, and delayed treatment.'''
     st.write(x)
+	
+
+
 	
 	
 	
